e7720469fc4e45767d7eb0b4bda14f93cd00258f
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2024-09-28 14:55:02 +0200

    whiteboardHandler: added checks for edge cases

--- apps/server/src/events/whiteboardHandler.ts
+++ apps/server/src/events/whiteboardHandler.ts
@@ -6,31 +6,46 @@ let storage: Whiteboard[] = [];
 
 export const whiteboardHandler = (io: Server, socket: Socket) => {
   socket.on("whiteboard:create", (callback: Function) => {
-    const whiteboard = new Whiteboard();
-    storage.push(whiteboard);
-    socket.join(whiteboard.id);
-    WhiteboardLogger.logCreate(whiteboard.id);
-    callback({ status: 201, whiteboard });
+    try {
+      const whiteboard = new Whiteboard();
+      storage.push(whiteboard);
+      socket.join(whiteboard.id);
+      WhiteboardLogger.logCreate(whiteboard.id);
+      callback({ status: 201, whiteboard });
+    } catch (error) {
+      callback({ status: 500, error: "Internal server error" });
+    }
   });
 
   socket.on("whiteboard:join", (id: string, callback: Function) => {
-    const whiteboard = storage.find((wb) => wb.id === id);
-    if (!whiteboard) return { status: 404, error: "Whiteboard not found" };
-    socket.join(whiteboard.id);
-    WhiteboardLogger.logJoin(whiteboard.id);
+    if (!id) callback({ status: 400, error: "Missing whiteboard ID" });
+    try {
+      const whiteboard = storage.find((wb) => wb.id === id);
+      if (!whiteboard) return callback({ status: 404, error: "Whiteboard not found" });
+      socket.join(whiteboard.id);
+      WhiteboardLogger.logJoin(whiteboard.id);
 
-    callback({
-      status: 200,
-      whiteboard,
-    });
+      callback({
+        status: 200,
+        whiteboard,
+      });
+    } catch (error) {
+      callback({ status: 500, error: "Internal server error" });
+    }
   });
 
   socket.on("whiteboard:draw", (id: string, line, callback: Function) => {
-    WhiteboardLogger.logDraw(io.sockets.adapter.rooms, socket.id, id);
-    storage.find((wb) => wb.id === id)?.content.push(line);
-    socket.to(id).emit("whiteboard:render", line);
-    callback({
-      status: 200,
-    });
+    if (!id) callback({ status: 400, error: "Missing whiteboard ID" });
+    if (!line) callback({ status: 400, error: "Bad request" });
+    try {
+      WhiteboardLogger.logDraw(io.sockets.adapter.rooms, socket.id, id);
+      storage.find((wb) => wb.id === id)?.content.push(line);
+      socket.to(id).emit("whiteboard:render", line);
+      callback({
+        status: 200,
+      });
+    } catch (error) {
+      callback({ status: 500, error: "Internal server error" });
+    }
   });
 };
