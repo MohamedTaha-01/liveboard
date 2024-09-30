5b944a7834085a88eb56faac03ecd129ad2ffbd4
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2024-09-30 14:49:17 +0200

    refactor: remove old whiteboard ID context

--- apps/client/src/components/DebugMenu/DebugMenu.tsx
+++ apps/client/src/components/DebugMenu/DebugMenu.tsx
@@ -1,12 +1,8 @@
 import { useContext } from "react";
 import { SocketContext } from "../../context/SocketProvider";
-import { WhiteboardContext } from "../../context/WhiteboardProvider";
-import { ToolSettingsContext } from "../../context/ToolSettingsProvider";
 
 function DebugMenu() {
   const { socket } = useContext(SocketContext)!;
-  const { whiteboardId } = useContext(WhiteboardContext)!;
-  const toolSettings = useContext(ToolSettingsContext);
 
   return (
     <div
@@ -22,31 +18,10 @@ function DebugMenu() {
         opacity: 0.8,
       }}>
       <h1 style={{ textAlign: "center", marginBottom: 8, fontSize: 20 }}>DEV MENU</h1>
-      <details>
-        <summary>
-          <h2 style={{ fontSize: 18 }}>Socket</h2>
-        </summary>
-        <ul style={{ listStyle: "none" }}>
-          <li>ID: {socket && socket.id}</li>
-        </ul>
-      </details>
-      <hr style={{ marginTop: 8, marginBottom: 8 }} />
-      <details>
-        <summary>
-          <h2 style={{ fontSize: 18 }}>Whiteboard</h2>
-        </summary>
-        <ul style={{ listStyle: "none" }}>
-          <li>Whiteboard ID: {whiteboardId && whiteboardId}</li>
-          <li>Owner: null</li>
-          <li>Visibility: null</li>
-        </ul>
-        <h3>Tool settings</h3>
-        <ul style={{ listStyle: "none" }}>
-          <li>Tool: {toolSettings.tool}</li>
-          <li>Size: {toolSettings.size}</li>
-          <li>Color: {toolSettings.color}</li>
-        </ul>
-      </details>
+      <h2 style={{ fontSize: 18 }}>Socket</h2>
+      <ul style={{ listStyle: "none" }}>
+        <li>ID: {socket && socket.id}</li>
+      </ul>
     </div>
   );
 }
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,17 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import SocketProvider from "./context/SocketProvider.tsx";
-import WhiteboardProvider from "./context/WhiteboardProvider.tsx";
 import { ToolSettingsProvider } from "./context/ToolSettingsProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <SocketProvider>
-      <WhiteboardProvider>
-        <ToolSettingsProvider>
-          <App />
-        </ToolSettingsProvider>
-      </WhiteboardProvider>
+      <ToolSettingsProvider>
+        <App />
+      </ToolSettingsProvider>
     </SocketProvider>
   </StrictMode>
 );
