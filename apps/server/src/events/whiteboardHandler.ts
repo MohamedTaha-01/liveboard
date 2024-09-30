9e967f1f1693b11a1d8c587d114601e521cfddb4
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2024-09-30 18:01:40 +0200

    fix: add return statements to stop execution on edge cases

--- apps/server/src/events/whiteboardHandler.ts
+++ apps/server/src/events/whiteboardHandler.ts
@@ -20,7 +20,7 @@ export const whiteboardHandler = (io: Server, socket: Socket) => {
   });
 
   socket.on("whiteboard:join", (id: string, callback: Function) => {
-    if (!id) callback({ status: 400, error: "Missing whiteboard ID" });
+    if (!id) return callback({ status: 400, error: "Missing whiteboard ID" });
     try {
       const whiteboard = storage.find((wb) => wb.id === id);
       if (!whiteboard) return callback({ status: 404, error: "Whiteboard not found" });
@@ -39,13 +39,36 @@ export const whiteboardHandler = (io: Server, socket: Socket) => {
     }
   });
 
-  socket.on("whiteboard:draw", (id: string, line, callback: Function) => {
-    if (!id) callback({ status: 400, error: "Missing whiteboard ID" });
-    if (!line) callback({ status: 400, error: "Bad request" });
+  socket.on("whiteboard:draw", (id: string, element, callback: Function) => {
+    if (!id) return callback({ status: 400, error: "Missing whiteboard ID" });
+    if (!element) return callback({ status: 400, error: "Bad request" });
     try {
       WhiteboardLogger.logDraw(io.sockets.adapter.rooms, socket.id, id);
-      storage.find((wb) => wb.id === id)?.content.push(line);
-      socket.to(id).emit("whiteboard:render", line);
+      // TODO: add check for edge case where wb does not exist
+      storage.find((wb) => wb.id === id)?.content.push(element);
+      socket.to(id).emit("whiteboard:render", element);
+      console.log(element);
+
+      callback({
+        status: 200,
+      });
+    } catch (error) {
+      callback({ status: 500, error: "Internal server error" });
+    }
+  });
+
+  socket.on("whiteboard:move-element", (id: string, element, callback: Function) => {
+    console.log("move element", element);
+
+    if (!id) return callback({ status: 400, error: "Missing whiteboard ID" });
+    if (!element) return callback({ status: 400, error: "Bad request" });
+    try {
+      const wb = storage.find((wb) => wb.id === id);
+      if (!wb) return callback({ status: 404, error: "Whiteboard not found" });
+      const index = wb?.content.findIndex((el) => el.id === element.id);
+      if (index === -1) callback({ status: 404, error: "Element not found" });
+      wb.content[index] = element;
+      socket.to(id).emit("whiteboard:render", element);
       callback({
         status: 200,
       });
