266729592581f9edeeab96c89131304dd183ab9b
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2024-09-30 11:04:23 +0200

    remove duplicated action

--- apps/client/src/pages/HomePage.tsx
+++ apps/client/src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import { useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { SocketContext } from "../context/SocketProvider";
-import { WhiteboardContext } from "../context/WhiteboardProvider";
 
 function HomePage() {
   const { socket } = useContext(SocketContext)!;
-  const { setWhiteboardId } = useContext(WhiteboardContext)!;
 
   const navigate = useNavigate();
   const wbCodeInputRef = useRef<HTMLInputElement>(null);
@@ -13,7 +11,6 @@ function HomePage() {
   const handleWhiteboardCreate = async () => {
     const res = await socket.emitWithAck("whiteboard:create");
     console.log("created:", res);
-    setWhiteboardId(res.whiteboard.id);
     navigate(`/whiteboards/${res.whiteboard.id}`);
   };
 
