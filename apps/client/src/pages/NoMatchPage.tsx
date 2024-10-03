865f6c25caaaee5546f045a0084fc00c868e74e6
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2024-10-03 10:21:43 +0200

    improve 404 page

--- apps/client/src/pages/NoMatchPage.tsx
+++ apps/client/src/pages/NoMatchPage.tsx
@@ -1,5 +1,13 @@
 function NoMatchPage() {
-  return <p>404</p>
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-4">
+      <h1 className="text-3xl">404</h1>
+      <p className="text-xl">
+        The page you're looking for is currently on a coffee break. Try again
+        later.
+      </p>
+    </div>
+  )
 }
 
 export default NoMatchPage
